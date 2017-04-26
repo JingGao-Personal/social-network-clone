96a343f89e027f038f4f9a83a5f655dbe4db98f4
Jing Gao <jinggao@Jings-MacBook-Pro-2.local>
2017-04-26 19:40:48 +1000

    Completed setting up Firebase integration

---
+++ social-network-clone/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  social-network-clone
+//
+//  Created by Jing Gao on 26/4/17.
+//  Copyright © 2017 Jing Gao. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
