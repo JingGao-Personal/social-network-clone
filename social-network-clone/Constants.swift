4136587dcc3b67bf03dd765db60803f6a275da1f
Jing Gao <jinggao@Jings-MacBook-Pro-2.local>
2017-04-27 10:43:48 +1000

    Sign in ui custom classes complete

---
+++ social-network-clone/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  social-network-clone
+//
+//  Created by Jing Gao on 27/4/17.
+//  Copyright © 2017 Jing Gao. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
