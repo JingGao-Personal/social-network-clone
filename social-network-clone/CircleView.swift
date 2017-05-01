f4533eef7ab7ecfc8eda844298715d78f9026029
Jing Gao <jinggao@Jings-MacBook-Pro-2.local>
2017-05-01 11:16:44 +1000

    feed ui complete

---
+++ social-network-clone/CircleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleView.swift
+//  social-network-clone
+//
+//  Created by Jing Gao on 1/5/17.
+//  Copyright © 2017 Jing Gao. All rights reserved.
+//
+
+import UIKit
+
+class CircleView: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
