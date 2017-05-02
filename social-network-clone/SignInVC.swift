b39ff1188d6e17f954a7de3087adb29ad38ed6d3
Jing Gao <jinggao@Jings-MacBook-Pro-2.local>
2017-05-02 22:46:06 +1000

    Complete creating Firebase DB users

--- social-network-clone/SignInVC.swift
+++ social-network-clone/SignInVC.swift
@@ -56,7 +56,8 @@ class SignInVC: UIViewController {
             } else {
                 print("Jing: Successfully authenticated with Firebase")
                 if let user = user {
-                    self.completeSignIn(id: user.uid)
+                    let userData = ["provider": credential.provider]
+                    self.completeSignIn(id: user.uid, userData: userData)
                 }
                 
             }
@@ -69,7 +70,8 @@ class SignInVC: UIViewController {
                 if error  == nil {
                     print("Jing: User authenticated with Firebase")
                     if let user = user {
-                        self.completeSignIn(id: user.uid)
+                        let userData = ["provider": user.providerID]
+                        self.completeSignIn(id: user.uid, userData: userData)
                     }
                 } else {
                     FIRAuth.auth()?.createUser(withEmail: email, password: pwd, completion: { (user, error) in
@@ -78,7 +80,8 @@ class SignInVC: UIViewController {
                         } else {
                             print("Jing: Successfully authenticated with Firebase")
                             if let user = user {
-                                self.completeSignIn(id: user.uid)
+                                let userData = ["provider": user.providerID]
+                                self.completeSignIn(id: user.uid, userData: userData)
                             }
                             
                         }
@@ -91,7 +94,8 @@ class SignInVC: UIViewController {
         }
     }
     
-    func completeSignIn(id: String) {
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirbaseDBUser(uid: id, userData: userData)
         let keychainResult = KeychainWrapper.standard.set(id, forKey: KEY_UID)
         print("Jing: Data saved to keychain \(keychainResult)")
         performSegue(withIdentifier: "goToFeed", sender: nil)
